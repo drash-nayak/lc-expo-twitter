df48b1b058d8fa9fdcaaa199dcd377c5e30b2b29
Drashtant Nayak <dnayak@steeleauto.com>
2023-01-01 16:08:13 -0700

    multiple screens with different types of navigations

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import HomeScreen from "./screens/HomeScreen";
+import NewTweetScreen from "./screens/NewTweetScreen";
+import TweetScreen from "./screens/TweetScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import SearchScreen from "./screens/SearchScreen";
+import NotificationsScreen from "./screens/NotificationsScreen";
+
+import {Ionicons} from '@expo/vector-icons';
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStackNavigator = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: true,
+                headerBackTitleVisible: true
+            }}>
+            <Stack.Screen name="Tab" component={TabNavigator}
+                          options={{headerShown: false}}
+            ></Stack.Screen>
+            <Stack.Screen name="New Tweet"
+                          component={NewTweetScreen}></Stack.Screen>
+            <Stack.Screen name="Tweet Screen" component={TweetScreen}
+            ></Stack.Screen>
+            <Stack.Screen name="Profile Screen" component={ProfileScreen}></Stack.Screen>
+        </Stack.Navigator>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const TabNavigator = () => {
+    return (
+        <Tab.Navigator screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false
+        }}>
+            <Tab.Screen name="Home1" component={HomeScreen}
+                        options={{
+                            tabBarIcon: ({color, size}) => (
+                                <Ionicons name="md-home" size={size} color={color}/>
+                            )
+                        }}
+            />
+            <Tab.Screen name="Search" component={SearchScreen}
+                        options={{
+                            tabBarIcon: ({color, size}) => (
+                                <Ionicons name="md-search" size={size} color={color}/>
+                            )
+                        }}
+            />
+            <Tab.Screen name="Notifications" component={NotificationsScreen}
+                        options={{
+                            tabBarIcon: ({color, size}) => (
+                                <Ionicons name="notifications" size={size} color={color}/>
+                            )
+                        }}
+            />
+        </Tab.Navigator>
+
+    );
+}
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home" screenOptions={{
+                headerShown: false
+            }}>
+                <Drawer.Screen name="Home" component={HomeStackNavigator}/>
+                <Drawer.Screen name="Settings" component={SettingsScreen}/>
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+}
---
+++ screens/NewTweetScreen.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+export default function NewTweetScreen() {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>New Tweet Screen</Text>
+        </View>
+    )
+}
